68adcef2f1514da38724fec8fd3a4e3e29a84e8e
ruslan <belozerzevruslan@gmail.com>
2019-04-05 15:33:49 +0300

    eddited

---
+++ src/js/quiz.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+  Marquiz.init({ 
+  id: '5ca48c3b7712390044c16395', 
+  autoOpen: false, 
+  autoOpenFreq: 'once', 
+  openOnExit: false 
+  });
+  });
